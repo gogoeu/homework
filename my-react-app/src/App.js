69f52806ba8e78bc72874c37546ff9f357763241
gogoeu <ko1331@naver.com>
2025-03-18 17:54:54 +0900

    d
    d

--- my-react-app/src/App.js
+++ my-react-app/src/App.js
@@ -10,8 +10,15 @@ function App() {
       <div className="black-nav">
         <h4>블로그임</h4>
       </div>
+      <div className="list">
+        <h4>글 제목</h4>
+        <p>2월 17일 발행</p>
+      </div>
     </div>
   );
 }
 
 export default App;
+
+
+/* 자료 잠깐 보관할 수 있는 변수랑 비슷한 state란 문법 사용법 useState, 상단에 자동으로 import 됨 */
